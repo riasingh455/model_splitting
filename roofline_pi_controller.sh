844fe063c1cbb0f9ea9b4e96025d3818c030e62b
animesh <animesh@localhost.localdomain>
2026-03-27 13:17:06 -0700

    emegency commit

--- roofline_pi_controller.sh
+++ roofline_pi_controller.sh
@@ -14,7 +14,7 @@ rng_gen () {
 node_select () {
         # valid_nodes=($(cat ${path_prefix}/full_sweep/${node_prefix}_middling))
         all_nodes=$(sinfo -N | grep "idle" | grep -v "idle\*" | grep "${node_prefix}" | awk '{print$1}')
-        node_list=( $(for i in ${all_nodes[@]}; do [[ $(cat ${path_prefix}/full_sweep/${node_prefix}_middling | grep -w "${all_nodes[$i]}" | wc -l) -gt 0 ]] && echo "${all_nodes[$i]}"; done) )
+        node_list=( $(for i in ${all_nodes[@]}; do [[ $(cat ${path_prefix}/full_sweep/${node_prefix}_middling | grep -w "${i}" | wc -l) -gt 0 ]] && echo "${i}"; done) )
         ind_arr=( $(rng_gen "${#node_list[@]}") )
         node_select=( $(for i in ${ind_arr[@]}; do echo "${node_list[$i]}"; done) )
         echo "${node_select[@]}"
@@ -30,17 +30,20 @@ then
 	echo "Missing arguments, expected: ./script.sh <model_type> (resnet18, mbv3_small, eb0) <model_split> (children, modules) <node_prefix> (bramble-x-y)" && exit
 fi
 
-for world in {2..10}
+for repeat in {1..10}
 do
-    nodes=( $(node_select) )
-    counter=0
-    for n in ${nodes[@]}
-    do
-    #     world=$world rank=$counter master=${nodes[0]} model_type=$model_type model_split=$model_split srun --nodelist=$n roofline_pi_script.sh &
-	# counter=$(( $counter+1 ))
-	echo $n, $world
-    done
-    echo "Selected ${nodes[@]} ${world}"
-    wait
-    break
+	for world in {2..10}
+	do
+    		nodes=( $(node_select) )
+    		counter=0
+    		for n in ${nodes[@]}
+    		do
+         		world=$world rank=$counter master=${nodes[0]} model_type=$model_type model_split=$model_split srun --nodelist=$n roofline_pi_script.sh &
+	 		counter=$(( $counter+1 ))
+			#echo $n, $world
+    		done
+    		echo "Selected ${nodes[@]} ${world}"
+    		wait
+    		#break
+	done
 done
