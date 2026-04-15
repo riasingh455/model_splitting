963552b7e5352f883fcb770b710b378ac50f6646
animesh <animesh@localhost.localdomain>
2026-04-15 08:34:52 -0700

    emergency commit

--- aot_splitter/group_heater.sh
+++ aot_splitter/group_heater.sh
@@ -47,30 +47,29 @@ then
 fi
 
 highest_flop_rank=( 0 0 1 1 2 )
-highest_mem_rank=( 0 1 2 2 3 )
-highest_mem_rank=( 0 0 0 3 4 )
 # for repeat in 3{1..10}
 for repeat in 2 3 4 5 6
 do
-    for fake_world in 2 3 4 5
+    #for fake_world in 2 3 4 5
+    for fake_world in 2
     do
-        for conc_val in 1 10
-        do
-            
+        # for (( conc_val=0; conc_val<$fake_world; conc_val++ ))
+        # do
+            conc_val=$fake_world
             nodes=( $(node_select) )
-            fake_rank_idx=$(( $fake_world-1 ))
-            #fake_rank=${highest_flop_rank[$fake_rank_idx]}
-            fake_rank=${highest_mem_rank[$fake_rank_idx]}
-            path_dst="${path_prefix}/logs/single_heat/${node_prefix}/${model_type}_${model_split}/${conc_val}_concurrency_level/${repeat}/${fake_world}_${fake_rank}/"
+        #     fake_rank_idx=$(( $fake_world-1 ))
+        #     fake_rank=${highest_flop_rank[$fake_rank_idx]}
+            
+            path_dst="${path_prefix}/logs/group_heat/${node_prefix}/${model_type}_${model_split}/${conc_val}_concurrency_level/${repeat}/"
             mkdir -p ${path_dst}
             waiters=()
             for (( n=0;n<${#nodes[@]};n++ ))
             do
-	    		ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pkill -9 roofline; pkill -9 python3; echo 'race4fun' | sudo -S sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches';"
-			    # timeout 10m ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pushd $path_prefix; node_prefix=$node_prefix world=$conc_val rank=$n master=${nodes[0]} model_type=$model_type model_split=$model_split bnum=$bnum ./roofline_pi_script_no_slurm_no_temp.sh &" &
-			    command="python3 single_runner.py --cores 4 --rank ${n} --world ${conc_val} --ip ${nodes[0]} --port 8123 --warmup 1 --batch-size 1 --batch-num ${bnum} --iters 10 --model-type ${model_type} --model-split-type ${model_split}"
-                command="${command} --fake_rank ${fake_rank} --fake_world ${fake_world}"
-                timeout 10m ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pushd $path_prefix/aot_splitter; source /home/animesh/model_splitting/pi-torch/bin/activate; ${command} > ${path_dst}/speed_chronos${nodes[$n]}.log &" &
+	    	ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pkill -9 roofline; pkill -9 python3; echo 'race4fun' | sudo -S sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches';"
+                # timeout 10m ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pushd $path_prefix; node_prefix=$node_prefix world=$conc_val rank=$n master=${nodes[0]} model_type=$model_type model_split=$model_split bnum=$bnum ./roofline_pi_script_no_slurm_no_temp.sh &" &
+		command="python3 single_runner.py --cores 4 --rank ${n} --world ${fake_world} --ip ${nodes[0]} --port 8123 --warmup 1 --batch-size 1 --batch-num ${bnum} --iters 10 --model-type ${model_type} --model-split-type ${model_split}"
+                command="${command} --fake_rank ${n} --fake_world ${fake_world}"
+                timeout 10m ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pushd $path_prefix/aot_splitter; source /home/animesh/model_splitting/pi-torch/bin/activate; ${command} > ${path_dst}/speed_chronos${nodes[$n]} &" &
                 waiters+=($!)
             done
             echo "Selected ${nodes[@]} ${world}"
@@ -81,6 +80,6 @@ do
 	    		ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pkill -9 roofline; pkill -9 python3; echo 'race4fun' | sudo -S sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches';"			    
             done
 
-        done
+        # done
     done
 done
