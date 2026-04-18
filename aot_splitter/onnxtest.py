8aa6281d7d8d3021fa344a234e3d4f7554274da2
animesh <animesh@localhost.localdomain>
2026-04-17 17:08:24 -0700

    exp logs

--- aot_splitter/onnxtest.py
+++ aot_splitter/onnxtest.py
@@ -6,6 +6,7 @@ import multiprocessing as mp
 from ast import literal_eval
 from datetime import datetime
 import time
+#from pathlib import Path
 
 def shape_shifter(numpy_shape, numpy_arr):
     slices=[] 
@@ -23,7 +24,7 @@ def shape_shifter(numpy_shape, numpy_arr):
     # final_arr = tuple(split_t)
     return split_t
 
-def silly_test(core, path, rank, iters, shape_dict):
+def silly_test(core, path, rank, iters, shape_dict, f_ts):
     # input_name = session.get_inputs()[0].name
     # print(shape_dict[rank])
     sess_opt = ort.SessionOptions()
@@ -35,14 +36,17 @@ def silly_test(core, path, rank, iters, shape_dict):
 
     # sess_opt.execution_mode = ort.ExecutionMode.ORT_SEQUENTIAL
     # sess_opt.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
-    x=np.empty(shape_dict[rank][0], dtype=np.float32)
+    x=[np.empty(shape_dict[rank][0], dtype=np.float32)]
     #network stuff technically here somewhere?
     #shapeshift to match input layer of model slice
     if len(shape_dict[rank])>2:
         x=shape_shifter(shape_dict[rank][2:], x)
 
     ort_sess = ort.InferenceSession(f'{path}/exe_split_{rank}.pte_quant.onnx', sess_options=sess_opt, providers=providers)
-    input_name = ort_sess.get_inputs()[0].name
+    
+    #fake_barrier(self_host, other_hosts, extra=core)
+    fake_barrier(f_ts)
+
     ts = datetime.now()
     print(f"{ts} Sync done -> model run start", flush=True)
     #print sync message -> do network sync here though
@@ -55,6 +59,7 @@ def silly_test(core, path, rank, iters, shape_dict):
         # print(core, outputs[0])
     total_end = time.perf_counter()
     print(core, round(total_end-total_start,3) )
+    #fake_barrier_rmv(self_host, core)
     
 # x, y = test_data[0][0], test_data[0][1]
 # print(ort_sess.get_modelmeta())
@@ -70,24 +75,50 @@ def str_to_dtype(code):
         raise ValueError(f"Unsupported dtype code: {code}")
     return mapping[code]
 
+def fake_barrier(f_ts):
+    while int(datetime.now().timestamp())<f_ts:
+        pass
+
+    #dname=f"/home/animesh/{self_host}_{extra}"
+    #Path.mkdir(Path(dname), exist_ok=False)
+    #check_list=[0]
+    #while sum(check_list)!=len(other_hosts):
+    #    check_list = [(1 if Path.is_dir(Path(f"/home/animesh/{h}_{extra}")) else 0) for h in other_hosts]
+    #Path.rmdir(Path(dname))
+
+def fake_barrier_rmv(self_host, extra=''):
+    pass
+    #dname=f"/home/animesh/{self_host}_{extra}"
+    #Path.rmdir(Path(dname))
+
 if __name__=="__main__":
     procs=[]
     iters = int(sys.argv[1])
     path = sys.argv[2]
     rank = int(sys.argv[3])
     cores= int(sys.argv[4])
+    f_ts = int(sys.argv[5])
+    #f_ts = datetime.now() + timedelta(seconds=t_delay)
+    #f_ts = (f_ts.timestamp()*1000)
+    #self_host=sys.argv[5]
+    #other_hosts=sys.argv[6].split(",")[:-1]
+
     if len(path)==0:
         exit
     f=open(f"{path}/stages.dict")
     line=f.readlines()[0].strip()
     shape_dict = literal_eval(line)
     numpy_shape= shape_dict[rank]
+    #hard sync with file system
+    #fake_barrier(self_host, other_hosts)
+    #fake_barrier(f_ts)
     #network, sync technically goes here? but skip for now
-
+    #print(datetime.now(), other_hosts)
+    #exit() 
     s_ts = datetime.now()
     # data = np.random.rand(batch_size, 224, 224, 3).astype(np.float32)
     for core in range(cores):
-        p = mp.Process(target=silly_test, args=(core, path, rank, iters, shape_dict, ))
+        p = mp.Process(target=silly_test, args=(core, path, rank, iters, shape_dict, f_ts, ))
         p.start()
         procs.append(p)
 
@@ -96,6 +127,9 @@ if __name__=="__main__":
     
     e_ts = datetime.now()
     print(f"{e_ts} Sync done -> model run start", flush=True)
+
+    #fake_barrier_rmv(self_host)
+
     print(f"echo 'race4fun' | sudo -S sh -c 'journalctl --since "+ f'"{s_ts}" --until "{e_ts}" -k -o short-iso '+"'| grep -i -E 'throttle|throttled|thermal|cpufreq|under-voltage|voltage'")
 
 # Print Result
