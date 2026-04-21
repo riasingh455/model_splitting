2e76f209ecad8403028a260d138308981b3a0b80
animesh <animesh@localhost.localdomain>
2026-04-20 22:38:32 -0700

    just the scripts

--- aot_splitter/model_rank_explorer_w_temp.sh
+++ aot_splitter/model_rank_explorer_w_temp.sh
@@ -23,7 +23,7 @@ node_select () {
 		then
 			hname=$(ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@$n_check 'hostname')
 			mem=$(ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@$n_check "echo 'race4fun' | sudo -S sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches';free | grep 'Mem'" | awk '{print $4}')
-			if [[ $n_check == $hname ]] && [[ $mem -ge 650000 ]]
+			if [[ $n_check == $hname ]]
 			then
 				node_list+=($n_check)
 			fi
@@ -51,15 +51,15 @@ fi
 
 
 conc_val=5
-nodes=( $(node_select) )
 
-for repeat in 0
+for repeat in {1..10}
+#for repeat in 0
 do
+    nodes=( $(node_select) )
     # for fake_world in 5 10 15
     # for fake_world in 15
     for fake_world in 5 2
     do
-        dt=$(date -d '+120 seconds' +%s)
         for (( fake_rank=0;fake_rank<$fake_world;fake_rank++ ))
         # for fake_rank in 4 13
         # for fake_rank in 0
@@ -67,43 +67,65 @@ do
                 fake_rank_idx=$(( $fake_world-1 ))
                 # #fake_rank=${highest_flop_rank[$fake_rank_idx]}
                 # fake_rank=${highest_mem_rank[$fake_rank_idx]}
-                path_dst="${path_prefix}/logs/single_group_heat_con/${node_prefix}/${model_type}_${model_split}_onnx/${repeat}/"
+                path_dst="${path_prefix}/logs/single_group_heat_con/${node_prefix}/${model_type}_${model_split}_onnx/all_dev/${repeat}/"
                 mkdir -p ${path_dst}
-                waiters=()
-                killers=()
-                for (( n=0;n<${#nodes[@]};n++ ))
-                do
-                    ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pkill -9 temp_speed; pkill -9 model_rank; pkill -9 python3; echo 'race4fun' | sudo -S sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches';"
-                    # timeout 10m ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pushd $path_prefix; node_prefix=$node_prefix world=$conc_val rank=$n master=${nodes[0]} model_type=$model_type model_split=$model_split bnum=$bnum ./roofline_pi_script_no_slurm_no_temp.sh &" &
-                    # command="python3 single_runner.py --custom --cores ${cores} --rank ${n} --world ${conc_val} --ip ${nodes[0]} --port 8123 --warmup 1 --batch-size 4 --batch-num 1 --iters 10 --model-type ${model_type} --model-split-type ${model_split}"
-                    # onnxtest.py 10 ./vit_modules_3_1_custom 0 4 ${dt}
-                    command="python3 onnxtest.py ./${model_type}_${model_split}_${fake_world}_1_custom 20 ${fake_rank} ${cores} ${dt}"
-                    # if [[ $wait_flag -eq 1 ]]
-                    # then
-                    #     command="python3 single_runner.py --custom --cores ${cores} --rank 0 --world 1 --ip ${nodes[$n]} --port 8123 --warmup 1 --batch-size 4 --batch-num 1 --iters 10 --model-type ${model_type} --model-split-type ${model_split}"   
-                    # fi
-                    # command="${command} --fake_rank ${fake_rank} --fake_world ${fake_world}"
-                    timeout 10m ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pushd $path_prefix/aot_splitter; source /home/animesh/model_splitting/pi-torch/bin/activate; ${command} > ${path_dst}/speed_chronos${nodes[$n]}_${fake_world}_${fake_rank}.log &" &
-                    # if [[ $wait_flag -eq 1 ]]
-                    # then
-                    #     wait $!
-                    # fi
-                    waiters+=($!)
-                    hcommand="mac_ver=0 log_path=${path_dst} ./temp_speed_reader.sh"
-                    full_hcommand="pushd ${path_prefix}/aot_splitter;${hcommand} &"
-                    ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${n} "${full_hcommand}" &
-                    killers+=($!)
-                done
-                echo "Selected ${nodes[@]} ${world}"
-                wait "${waiters[@]}"
-                #cleanup
-                for (( n=0;n<${#nodes[@]};n++ ))
-                do
-                    ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "kill -9 ${killers[$n]}; pkill -9 temp_speed; pkill -9 model_rank; pkill -9 python3;"
-                    # $(tail -n 1 ${path_dst}/speed_chronos${nodes[$n]}_${fake_rank}.log) > ${path_dst}/speed_journal${nodes[$n]}_${fake_rank}.log
-                    ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "echo 'race4fun' | sudo -S sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches';"
-                done
+		for wait_flag in 1 0
+		do
+                	waiters=()
+                	killers=()
+        		dt=$(date -d '+60 seconds' +%s)
+			if [[ $wait_flag -eq 1 ]]
+			then
+				dt=$(date -d '+1 seconds' +%s)
+				path_dst="${path_prefix}/logs/single_group_heat_con/${node_prefix}/${model_type}_${model_split}_onnx/per_dev/${repeat}/"
+				mkdir -p ${path_dst}
+		
+			else
+                		path_dst="${path_prefix}/logs/single_group_heat_con/${node_prefix}/${model_type}_${model_split}_onnx/all_dev/${repeat}/"
+			fi
 
+                	for (( n=0;n<${#nodes[@]};n++ ))
+                	do
+                    		ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pkill -9 temp_speed; pkill -9 model_rank; pkill -9 python3; echo 'race4fun' | sudo -S sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches';"
+                    		# timeout 10m ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pushd $path_prefix; node_prefix=$node_prefix world=$conc_val rank=$n master=${nodes[0]} model_type=$model_type model_split=$model_split bnum=$bnum ./roofline_pi_script_no_slurm_no_temp.sh &" &
+                    		# command="python3 single_runner.py --custom --cores ${cores} --rank ${n} --world ${conc_val} --ip ${nodes[0]} --port 8123 --warmup 1 --batch-size 4 --batch-num 1 --iters 10 --model-type ${model_type} --model-split-type ${model_split}"
+                    		# onnxtest.py 10 ./vit_modules_3_1_custom 0 4 ${dt}
+                    		
+				hcommand="mac_ver=0 log_path=${path_dst} ./temp_speed_reader.sh"
+                    		full_hcommand="pushd ${path_prefix}/aot_splitter;${hcommand} &"
+                    		
+				command="python3 onnxtest.py 10 ./${model_type}_${model_split}_${fake_world}_1_custom ${fake_rank} 4 ${dt}"
+                    		# command="${command} --fake_rank ${fake_rank} --fake_world ${fake_world}"
+                    		timeout 10m ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pushd $path_prefix/aot_splitter; source /home/animesh/model_splitting/pi-torch/bin/activate; ${command} > ${path_dst}/speed_chronos${nodes[$n]}_${fake_world}_${fake_rank}.log &" &
+                    		wait_pid=$!
+				ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "${full_hcommand}" &
+                    		kill_pid=$!
+				if [[ $wait_flag -eq 1 ]]
+                    		then
+                    		     wait $wait_pid
+				     kill -9 $kill_pid
+				     #continue
+                    		fi
+                    		waiters+=($wait_pid)
+                    		killers+=($kill_pid)
+                	done
+                	echo "Selected ${nodes[@]} ${world}"
+			if [[ $wait_flag -eq 0 ]]
+			then
+                		wait "${waiters[@]}"
+			fi
+			#cleanup
+                	for (( n=0;n<${#nodes[@]};n++ ))
+                	do
+				if [[ $wait_flag -eq 0 ]]
+				then
+					ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "kill -9 ${killers[$n]};"
+				fi
+                    		ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "pkill -9 temp_speed; pkill -9 model_rank; pkill -9 python3;"
+                    		# $(tail -n 1 ${path_dst}/speed_chronos${nodes[$n]}_${fake_rank}.log) > ${path_dst}/speed_journal${nodes[$n]}_${fake_rank}.log
+                    		ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no animesh@${nodes[$n]} "echo 'race4fun' | sudo -S sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches';"
+                	done
+		done
         done
     done
 done
